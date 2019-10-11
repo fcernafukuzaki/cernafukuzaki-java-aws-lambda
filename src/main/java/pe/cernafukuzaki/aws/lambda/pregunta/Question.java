8f8e501f9db4fa8539ac27ac33b02a1957f95638
Francisco Cerna Fukuzaki <fcernafukuzaki@gmail.com>
2019-10-11 15:59:22 -0500

    Uso de API Gateway

--- src/main/java/pe/cernafukuzaki/aws/lambda/pregunta/Question.java
+++ src/main/java/pe/cernafukuzaki/aws/lambda/pregunta/Question.java
@@ -2,14 +2,20 @@ package pe.cernafukuzaki.aws.lambda.pregunta;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
 
-public class Question implements RequestHandler<Object, String> {
+public class Question implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
 
-    @Override
-    public String handleRequest(Object input, Context context) {
-        context.getLogger().log("Input: " + input);
-        String output = "Hi! My name is " + input + "! Nice to meet you.";
-        return output;
-    }
+	@Override
+	public APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent input, Context context) {
+		String output = "Hi! My name is " + input.getBody() + "! Nice to meet you.";
+    	
+    	APIGatewayProxyResponseEvent responseEvent = new APIGatewayProxyResponseEvent();
+        responseEvent.setStatusCode(200);
+        responseEvent.setBody(output);
+        
+        return responseEvent;
+	}
 
 }
