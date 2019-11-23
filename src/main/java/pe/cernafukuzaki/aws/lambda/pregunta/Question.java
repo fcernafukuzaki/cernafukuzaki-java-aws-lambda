dc5086a344c762968aee1466fc2732ac4fa5200d
fcernafukuzaki <fcernaf@gmail.com>
2019-11-23 18:09:55 -0500

    Log for Lambda Function

--- src/main/java/pe/cernafukuzaki/aws/lambda/pregunta/Question.java
+++ src/main/java/pe/cernafukuzaki/aws/lambda/pregunta/Question.java
@@ -1,21 +1,39 @@
 package pe.cernafukuzaki.aws.lambda.pregunta;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
 
 public class Question implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
 
+	// Initialize the Log4j logger.
+	static final Logger logger = LogManager.getLogger(Question.class);
+	
 	@Override
 	public APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent input, Context context) {
 		String output = "Hi! My name is " + input.getBody() + "! Nice to meet you.";
-    	
-    	APIGatewayProxyResponseEvent responseEvent = new APIGatewayProxyResponseEvent();
-        responseEvent.setStatusCode(200);
-        responseEvent.setBody(output);
-        
-        return responseEvent;
+		System.out.println("SYSTEM OUT PRINTLN [INICIO] :: Esta es una prueba de Lambda Function!");
+		LambdaLogger lambdaLogger = context.getLogger();
+		lambdaLogger.log("LambdaLogger [INICIO] :: Esta es una prueba de Lambda Function!");
+		logger.info("LambdaLogger [INICIO] :: Esta es una prueba de Lambda Function!");
+		APIGatewayProxyResponseEvent responseEvent = new APIGatewayProxyResponseEvent();
+		
+		System.out.println("SYSTEM OUT PRINTLN [INTERMEDIO]");
+		lambdaLogger.log("LambdaLogger [INTERMEDIO]");
+		logger.error("LambdaLogger [INTERMEDIO]");
+		
+		responseEvent.setStatusCode(200);
+		responseEvent.setBody(output);
+		
+		System.out.println("SYSTEM OUT PRINTLN [FIN]");
+		lambdaLogger.log("LambdaLogger [FIN]");
+		logger.info("LambdaLogger [FIN]");
+		return responseEvent;
 	}
 
 }
